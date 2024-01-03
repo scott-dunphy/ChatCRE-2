cffb966674161c691031ec8d613629639a1c7b93
scott-dunphy <133469585+scott-dunphy@users.noreply.github.com>
2024-01-02 22:26:40 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@
 
 from langchain.chat_models import ChatOpenAI
 import os
+from pathlib import path
 import pinecone
 import streamlit as st
 from langchain.chains import RetrievalQA
@@ -44,6 +45,17 @@ docsearch = Pinecone.from_existing_index(index_name, embeddings)
 
 chain = load_qa_chain(llm, chain_type="stuff")
 
+def text_to_speech(text):
+    speech_file_path = Path(__file__).parent / "speech.mp3"
+    response = client.audio.speech.create(
+      model="tts-1",
+      voice="alloy",
+      input=text
+    )
+    pass
+
+response.stream_to_file(speech_file_path)
+
 def chatcre(query):
     docs = docsearch.similarity_search(query, k=5)
     return chain.run(input_documents=docs, question=query)
@@ -54,6 +66,14 @@ st.title('ChatCRE')
 query_input = st.text_input("Ask questions about the leases, property updates, and loan agreement. Be patient while it runs.")
 try:
     write_value = chatcre(query_input)
+    if st.button("Read Aloud"):
+        text_to_speech(text)
+    
+        # Check if the output.mp3 file exists
+        if os.path.isfile('speech.mp3'):
+            st.audio('speech.mp3', format='audio/mp3')
+        else:
+            st.error("Error generating audio.")
 except:
     write_value = "Try this prompt: Is there anything concerning in the property updates?"
 st.write(write_value)
